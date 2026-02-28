b6b1dfa3dcffd5ae4fea03e233fb336f68b778dd
whyhilde <whyhilde@gmail.com>
2026-02-28 21:37:31 +0500

    feat: improve display

--- src/display.go
+++ src/display.go
@@ -1,11 +1,32 @@
 package src
 
 import (
+	"context"
 	"fmt"
+
+	"github.com/docker/docker/client"
 )
 
-func Display(info []string) {
-	logo := []string {
+func FormatInfoLines(key string, value string, spaces string, cfg *Config) string {
+	line := cfg.KeyCol + cfg.Bold + key + cfg.Sep + spaces + cfg.Reset + value
+	return line
+}
+
+func CollectDockerInfo(cli *client.Client, ctx context.Context, cfg *Config) []string {
+	var lines []string
+
+	if ver, api, os, arch, err := GetDockerVersion(cli, ctx); err == nil {
+		lines = append(lines, fmt.Sprint(FormatInfoLines("Version", ver, " ", cfg)))
+		lines = append(lines, fmt.Sprint(FormatInfoLines("API", api, "     ", cfg)))
+		lines = append(lines, fmt.Sprint(FormatInfoLines("OS", os, "      ", cfg)))
+		lines = append(lines, fmt.Sprint(FormatInfoLines("Arch", arch, "    ", cfg)))
+	}
+
+	return lines
+}
+
+func Display(info []string, cfg *Config) {
+	logo := []string{
 		"               ###       ##    ",
 		"       ### ### ###       ######",
 		"   ### ### ### ### ###   ####  ",
@@ -25,7 +46,7 @@ func Display(info []string) {
 	for i := 0; i < maxLines; i++ {
 		logoLine := ""
 		if i < len(logo) {
-			logoLine = logo[i]
+			logoLine = cfg.LogoCol + cfg.Bold + logo[i] + cfg.Reset
 		}
 
 		infoLine := ""
@@ -35,6 +56,4 @@ func Display(info []string) {
 
 		fmt.Printf("%s   %s\n", logoLine, infoLine)
 	}
-
-	fmt.Println()
 }
